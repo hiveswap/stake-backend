74aa193e2c649c2395f1897b863ace63ad17e070
goaisd <goaisd@outlook.com>
2024-03-22 17:11:25 +0800

    feat/auth: add auth

---
+++ src/auth/auth.guard.ts
@@ -0,0 +1,33 @@
+import {
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { Request } from 'express';
+import * as bcrypt from 'bcrypt';
+
+// we use bcrypt to hash the request body as token
+@Injectable()
+export class AuthGuard implements CanActivate {
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const request = context.switchToHttp().getRequest();
+    const token = this.extractTokenFromHeader(request);
+    if (!token) {
+      throw new UnauthorizedException();
+    }
+    const body = JSON.stringify(request.body);
+    const success = await bcrypt.compare(body, token);
+
+    if (!success) {
+      throw new UnauthorizedException();
+    }
+    request['user'] = token;
+    return true;
+  }
+
+  private extractTokenFromHeader(request: Request): string | undefined {
+    const [type, token] = request.headers.authorization?.split(' ') ?? [];
+    return type === 'Bearer' ? token : undefined;
+  }
+}
