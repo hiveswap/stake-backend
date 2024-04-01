4f7e8457acc9017882d7775f68b645c81d860430
Hive Wax <hive-wax@outlook.com>
2024-04-02 00:11:57 +0800

    feat: add lp

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -49,4 +49,20 @@ export const tokenAddrToPrice: Map<string, BigNumber> = new Map([
   ['0x33daba9618a75a7aff103e53afe530fbacf4a3dd', new BigNumber(1)],
   // ETH
   ['0x05ab928d446d8ce6761e368c8e7be03c3168a9ec', new BigNumber(3543.96)],
+  // stMAPO
+  ['0x9bd1e0a3a727d0d4f4e9a6d59022e071ddc79924', new BigNumber(0.034)],
+  // LSGS
+  ['0x756af1d3810a01d3292fad62f295bbcc6c200aea', new BigNumber(0.0001584)],
+  // BTC
+  ['0xb877E3562a660C7861117c2f1361A26ABaF19bEB', new BigNumber(70234)],
+  // M-BTC
+  ['0x1d22c0ab633f393c84a98cf4f2fad10ba47bb7b3', new BigNumber(70234)],
+  // ROUP
+  ['0x5a1c3f3aaE616146C7b9bf9763E0ABA9bAFc5eaE', new BigNumber(0.0028332)],
+  // SolvBTC
+  ['0x7eB8B1fE3EE3287FD5864e50f32322ce3285b39D', new BigNumber(70234)],
+  // iUSD
+  ['0x61899CE1396FF351e5fdb9c8AD36FeE9411c73c2', new BigNumber(1)],
+  // STST
+  ['0xf5a59f961a8e86285dae2e45ac4ae50e4e47ba97', new BigNumber(1)],
 ]);
