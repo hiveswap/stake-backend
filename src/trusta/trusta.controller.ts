ea5b1b8ff4b2c97613491d1e4936e8de017fdb9e
Hive Wax <hive-wax@outlook.com>
2024-06-05 11:41:25 +0800

    feat: add merkle api

--- src/trusta/trusta.controller.ts
+++ src/trusta/trusta.controller.ts
@@ -27,8 +27,8 @@ export class TrustaController {
   async getAirdropMerkle(@Param('address') address: string) {
     console.log(address);
     const claim = this.merkleM.get(address.toLowerCase());
-    const index = BigInt(claim?.index ?? 0n);
-    const amount = BigInt(claim?.amount ?? 0n);
+    const index = claim?.index;
+    const amount = claim?.amount;
     const proof = claim?.proof ?? [''];
     return {
       index,
